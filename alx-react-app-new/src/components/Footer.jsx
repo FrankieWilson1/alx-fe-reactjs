fe45b4132b43b50a779a0141afa6e90ed1f0fc4a
frankiewilson1 <frankwilliamsugwu@gmail.com>
2026-01-16 15:29:49 -0800

    add a new folder for the second app version

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer style={{ position: 'absolute',  bottom: '0', left: '0', right: '0' }}>
+        <footer style={{ position: 'absolute',  bottom: '0', left: '0', right: '0', backgroundColor: 'navy', color: 'white'}}>
             <p>@ 2023 City Lovers</p>
         </footer>
     );
