b1f8ca56058ebbda40c8a7d710af1e766212ed6c
frankiewilson1 <frankwilliamsugwu@gmail.com>
2026-01-18 12:47:57 -0800

    move Navbar component to components folder

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,7 +4,8 @@ import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Services from './Services';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
+
 
 const RootLayout = () => {
   return (
