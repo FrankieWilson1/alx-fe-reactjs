9290b1ce59e9c64a51a85c5c151e54ead80e172d
frankiewilson1 <frankwilliamsugwu@gmail.com>
2026-01-29 15:54:28 -0800

    Setting Up a React Project for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        GitHub User Search
+      </h1>
+      <p className="mt-4 text-gray-700">
+        Ready to find some developers?
+      </p>
+    </div>
+  )
+}
