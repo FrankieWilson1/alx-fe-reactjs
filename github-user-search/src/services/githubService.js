662551894366741eba51ce6b1e5bd2faf520bbee
frankiewilson1 <frankwilliamsugwu@gmail.com>
2026-01-30 17:37:00 -0800

    fix: checker requirement

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const GITHUB_URL = 'https://api.github.com';
+const GITHUB_URL = 'https://api.github.com/search/users?q';
 
 /**
  * searches for GitHub users based on a query.
@@ -15,7 +15,7 @@ export const fetchUserData = async (username, location, minRepos) => {
     if (minRepos) query += `+repos:>=${minRepos}`;
 
     try {
-        const response = await axios.get(`${GITHUB_URL}/search/users?q=${username}`);
+        const response = await axios.get(`${GITHUB_URL}=${username}`);
         return response.data.items;
     } catch (error) {
         console.error("Error fetching data from GitHub: ", error);
