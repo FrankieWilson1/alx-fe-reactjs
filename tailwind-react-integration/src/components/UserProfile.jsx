c923c086a58b85b5e69b9988dd3d428245660a90
frankiewilson1 <frankwilliamsugwu@gmail.com>
2026-02-08 06:42:36 -0800

    feat: add responsivity to the profile card

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,13 +2,13 @@
 
 function UserProfile() {
     return (
-        <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+        <div className="user-profile bg-gray-100 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 max-w-xs max-w-sm">
             <img src="img/profile.jpg"
                 alt="User"
-                className="rounded-full w-36 h-36 mx-auto object-cover border-4 border-white shadow-md"
+                className="rounded-full sm:w-24 sm:h-24 md:w-36 mx-auto object-cover border-4 border-white shadow-md"
             />
-            <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-            <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+            <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+            <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
         </div>
     );
 }
